cf6bc4329c1dd733300d26b09930778bb0577928
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-02-21 23:27:30 +0530

    Add new dependencies and configure Auth0 integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Home, Landing } from "./pages";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  return (
+  const {isAuthenticated} = useAuth0('');
+   return (
     <div>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={isAuthenticated ? <Landing /> : <Home/>} exact />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-sl8rym8wgasakdyg.us.auth0.com"
+      clientId="dLyO8soCwIpRstigfJM1WAZMDJLzGihM"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
